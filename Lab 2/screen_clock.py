d259a9d47f1c160712e8398baac2fab4e6b78802
yyyyannisz <107211991+yyyyannisz@users.noreply.github.com>
2025-09-28 14:26:18 -0400

    adjust font

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -54,7 +54,7 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 15)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
