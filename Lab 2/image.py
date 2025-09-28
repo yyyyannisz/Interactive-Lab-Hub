ee8f9c8109b742126a157fc08fd16894bc006d28
yyyyannisz <107211991+yyyyannisz@users.noreply.github.com>
2025-09-28 11:33:51 -0400

    Added button image switching

--- Lab 2/image.py
+++ Lab 2/image.py
@@ -10,7 +10,7 @@ not support PIL/pillow (python imaging library)!
 
 Author(s): Melissa LeBlanc-Williams for Adafruit Industries
 """
-
+import time
 import digitalio
 import board
 from PIL import Image, ImageDraw
@@ -65,37 +65,54 @@ if disp.rotation % 180 == 90:
 else:
     width = disp.width  # we swap height/width to rotate it to landscape!
     height = disp.height
-image = Image.new("RGB", (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-disp.image(image)
-
-image = Image.open("red.jpg")
+# Turn on backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+# Setup Buttons
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonA.switch_to_input(pull=digitalio.Pull.UP)
 
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonB.switch_to_input(pull=digitalio.Pull.UP)
 
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
+# Helper function to load and display image
+def show_image(filename):
+    image = Image.open(filename)
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
 
-# Display image.
-disp.image(image)
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
 
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    disp.image(image)
+
+# Initial image
+current_image = "red.jpg"
+show_image(current_image)
+
+# Main loop
+print("Press Button A for red.jpg, Button B for blue.jpg. Press Ctrl+C to exit.")
+while True:
+    if not buttonA.value:  # Button A is pressed
+        if current_image != "red.jpg":
+            show_image("red.jpg")
+            current_image = "red.jpg"
+            time.sleep(0.3)  # Debounce
+
+    if not buttonB.value:  # Button B is pressed
+        if current_image != "blue.jpg":
+            show_image("blue.jpg")
+            current_image = "blue.jpg"
+            time.sleep(0.3)  # Debounce
